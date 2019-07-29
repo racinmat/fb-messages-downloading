9d6ad66da16a8c56636d7cc86986f9b39907ebc1
MatejRacinsky <matej.racinsky@foxconn4tech.com>
2019-07-29 17:04:02 +0200

    modified template to support time grouping

--- csv_to_html.py
+++ csv_to_html.py
@@ -12,20 +12,26 @@ if __name__ == '__main__':
     # df = pd.read_csv('data/Wongwahmeni-total.csv')
     df = pd.read_csv('data/example.csv')
     df['DateTime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
+    df = df.set_index('DateTime', drop=False)
 
     data = []
     print('loaded data')
-    prev_row = None
-    for index, row in df.iterrows():
-        # date = datetime.strptime(row['Date'], '%Y-%m-%d %I:%M %p').strftime("%#d.%#m.,%H:%M")     # not working now, probably changed format
-        date = row['DateTime'].strftime("%#d.%#m.,%H:%M")
-        # merging messages from same person
-        if prev_row is not None and row['UserID'] == prev_row['UserID'] and (
-                row['DateTime'] - prev_row['DateTime']).seconds <= 60:
-            data[-1] = (data[-1][0], data[-1][1], data[-1][2] + '<br/>' + row['MessageBody'])
-        else:
-            data.append((date, row['UserName'], row['MessageBody']))
-        prev_row = row
+    for name, group in df.groupby(pd.Grouper(freq='5Min')):
+        timeslot = []
+        prev_row = None
+        for index, row in group.iterrows():
+            # date = datetime.strptime(row['Date'], '%Y-%m-%d %I:%M %p').strftime("%#d.%#m.,%H:%M")     # not working now, probably changed format
+            # date = row['DateTime'].strftime("%#d.%#m.,%H:%M")
+            # merging messages from same person
+            if prev_row is not None and row['UserID'] == prev_row['UserID'] and (
+                    row['DateTime'] - prev_row['DateTime']).seconds <= 60:
+                prev_data = timeslot[-1]
+                timeslot[-1] = (prev_data[0], prev_data[1] + '<br/>' + row['MessageBody'])
+            else:
+                timeslot.append((row['UserName'], row['MessageBody']))
+            prev_row = row
+        data.append((name.strftime("%#d.%#m.%#Y,%H:%M"), timeslot))
+
     print('data converted')
     # template.stream(rows=data).dump('data/Wongwahmeni-total.html')
     template.stream(rows=data).dump('data/example.html')
